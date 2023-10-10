5f14d99c5a7442a65e316aef1a0765def49ea222
nao52 <naoki.52abc@icloud.com>
2023-10-10 13:11:12 +0900

    記事の編集機能を実装

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,6 +21,15 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to articles_url
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def article_params

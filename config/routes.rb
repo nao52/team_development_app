37cf28430a1cad4bfdb419376fc9dff60a0ff0f4
野村恵理奈 <n.erina@nomuraerinanoMacBook-Pro.local>
2023-10-18 21:38:53 +0900

    ルーティング作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   resources :users, only: %i[new create]
   resources :articles
+
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: 'user_sessions#create'
+  delete 'logout', to: 'user_sessions#destroy'
 end

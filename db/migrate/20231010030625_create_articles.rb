fd3a50da10f452968b73a78e43453e715160c42a
桐山恵輔 <kiriyamakeisuke@kiriyamakeisukenoMacBook-Air.local>
2023-10-23 09:07:09 +0900

    フラッシュメッセージの修正

--- db/migrate/20231010030625_create_articles.rb
+++ db/migrate/20231010030625_create_articles.rb
@@ -1,6 +1,7 @@
 class CreateArticles < ActiveRecord::Migration[7.0]
   def change
     create_table :articles do |t|
+      t.references :user, null: false, foreign_key: true
       t.string :title,  null: false
       t.text :body,     null: false
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema[7.0].define(version: 2023_10_13_002746) do
   create_table "articles", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title", null: false
@@ -30,17 +29,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_13_002746) do
     t.index ["article_id"], name: "index_comments_on_article_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
-=======
-ActiveRecord::Schema[7.0].define(version: 2023_10_15_111523) do
-  create_table "articles", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "title", null: false
-    t.text "body", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "user_id", null: false
-    t.index ["user_id"], name: "index_articles_on_user_id"
-  end
->>>>>>> main
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email", null: false
@@ -54,9 +42,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_15_111523) do
   end
 
   add_foreign_key "articles", "users"
-<<<<<<< HEAD
   add_foreign_key "comments", "articles"
   add_foreign_key "comments", "users"
-=======
->>>>>>> main
 end

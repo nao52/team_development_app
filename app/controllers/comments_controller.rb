16ea52e125cc69db726e70114ddd56197da72bb9
桐山恵輔 <kiriyamakeisuke@kiriyamakeisukenoMacBook-Air.local>
2023-10-31 09:45:36 +0900

    コメント編集機能の削除

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,20 +8,6 @@ class CommentsController < ApplicationController
     end
   end
 
-  def edit
-    comment = current_user.comments.find(params[:id])
-  end
-
-  def update
-    comment = current_user.comments.find(params[:id])
-    if comment.update(comment.params)
-      redirect_to article_path(comment.article), success: t('.success')
-    else
-      flash.now[:danger] = t('.failure')
-      render :edit, status: :unprocessable_entity
-    end
-  end
-
   def destroy
     comment = current_user.comments.find(params[:id])
     comment.destroy!
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,7 @@
 class Article < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
+  has_many :comment_users, through: :comments, source: :user
 
   validates :title, presence: true
   validates :body,  presence: true
